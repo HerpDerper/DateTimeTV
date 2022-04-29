f8e29887ab3c736a423b5f5abe3691726d0666b4
eduardkutermin@gmail.com <eduardkutermin@gmail.com>
2022-04-29 23:58:12 +0300

    switch code

--- app/src/main/java/com/example/smthtv/MainActivity.java
+++ app/src/main/java/com/example/smthtv/MainActivity.java
@@ -1,16 +1,11 @@
 package com.example.smthtv;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.app.TimePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.DateUtils;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.fragment.app.FragmentActivity;
@@ -37,24 +32,18 @@ public class MainActivity extends FragmentActivity {
             dateTime.set(Calendar.SECOND, 0);
             edtSeconds.setText(DateUtils.formatDateTime(getApplicationContext(), dateTime.getTimeInMillis(), DateUtils.FORMAT_SHOW_TIME));
         };
-        edtSeconds.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    new TimePickerDialog(MainActivity.this, picker,
-                            dateTime.get(Calendar.HOUR_OF_DAY),
-                            dateTime.get(Calendar.MINUTE), true).show();
-                }
-                return true;
+        edtSeconds.setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                new TimePickerDialog(MainActivity.this, picker,
+                        dateTime.get(Calendar.HOUR_OF_DAY),
+                        dateTime.get(Calendar.MINUTE), true).show();
             }
+            return true;
         });
         btnStartTimer.setOnClickListener(view -> {
             long seconds = dateTime.getTimeInMillis();
             if (dateTime.getTimeInMillis() < System.currentTimeMillis()) seconds += 86400000;
             Toast.makeText(getApplicationContext(), "Будильник установлен на " + DateUtils.formatDateTime(getApplicationContext(), seconds, DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_TIME), Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(MainActivity.this, Alarm.class);
-            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-            alarmManager.set(AlarmManager.RTC_WAKEUP, seconds, PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0));
         });
     }
 }
